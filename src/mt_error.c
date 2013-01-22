d4e19c3a165236846388d67298816a474dfacf76
Chris Knadler <takeshi91k@gmail.com>
2013-01-22 09:12:59 -0800

    Adds basic MtError scaffold

---
+++ src/mt_error.c
@@ -0,0 +1,59 @@
+#include "mt_error.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Private protos
+static void invalid_conf_option(char* op);
+static void invalid_conf_data(char* op);
+
+void mt_error_emit(MtError e, ...)
+{
+  va_list argp;
+  va_start(argp, e);
+
+  // Emit error message
+  switch (e)
+  {
+    case INVALID_CONF_OPTION:
+      invalid_conf_option(va_arg(argp, char*));
+      break;
+
+    case INVALID_CONF_DATA:
+      invalid_conf_data(va_arg(argp, char*));
+      break;
+
+    default:
+      assert(false);
+      break;
+  }
+
+  // TODO: Free memory
+  // Exit is called directly after this
+  // All memory should be freed application wide
+  va_end(argp);
+
+  // Exit with proper error code
+  // TODO: you may want to change this to `exit(1)`
+  // Not exactly sure of the implications of exiting with arbitrary codes
+  exit(e);
+}
+
+static void
+invalid_conf_option(char* op)
+{
+  fprintf(stderr, "Invalid config option: %s\n", op);
+}
+
+// TODO:
+// Right now when bad data is passed to a config option
+// This just outputs the option and the fact that it is malformatted
+// Eventually, it would be nice to print the malformatted data as a string
+// in addition to the op.
+static void
+invalid_conf_data(char* op)
+{
+
+}
--- src/mt_error.h
+++ src/mt_error.h
@@ -1,4 +1,18 @@
 #ifndef MT_ERROR_H
 #define MT_ERROR_H
 
+#include <stdarg.h>
+
+/**
+ * A unified error handler for MarkTab
+ */
+
+typedef enum MtError
+{
+  INVALID_CONF_OPTION,
+  INVALID_CONF_DATA
+} MtError;
+
+void mt_error_emit(MtError e, ...);
+
 #endif // MT_ERROR_H
